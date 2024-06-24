a261df41eb8667e0c0150a21f55c0b852724b7d8
2110204107 <2110204107@gap.wzu.edu.tw>
2024-06-24 16:50:07 +0800

    Update phonetic.py

--- phonetic.py
+++ phonetic.py
@@ -2,37 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 #word = input( '請輸入中文字:' )
 def read(word):
-    url = f'https://dictionary.cambridge.org/dictionary/english-chinese-traditional/{word}'
+    url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
 
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
-    headers = {'User-Agent': user_agent}
-
-    print(word+'的查詢結果：')
-
+    html = requests.get( url )
+    bs = BeautifulSoup(html.text,'lxml')
+    data = bs.find('table', id='searchL')
     try:
-        web_request = requests.get(url, headers=headers)
-        bs = BeautifulSoup(web_request.text, "html.parser")
-
-        means = bs.find_all("div", class_="entry-body__el")
-
-
-        if means:
-            for mean in means:
-                posgram = mean.find('div', class_="posgram").text.strip()
-                print(f'{posgram}:')
-
-                def_bodies = mean.find_all("div", class_="def-body")
-
-                if def_bodies:
-                    i = 1
-                    for def_body in def_bodies:
-                        translation = def_body.find('span').text.strip()
-                        print(f'{i}. {translation}')
-                        i += 1
-                else:
-                    print('查無此字。')
-        else:
-            print('查無此字。')
-
-    except error:
-        print('查無此字')
+        row = data.find_all('tr')[2]
+        chinese = row.find('cr').text
+        phones = row.find_all('code')
+        phone = [e.text for e in phones]
+        s = " ".join( phone )
+        # s = row.find('sub')
+        return chinese + ' → ' + s 
+    except:
+        return '查無此字'
